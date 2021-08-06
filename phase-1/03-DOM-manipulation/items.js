fa44e2f91c185bf38b68b7bbab91746b297700fd
Aysan Isayo <aisayo@icloud.com>
2021-08-06 12:52:07 -0700

    code

---
+++ phase-1/03-DOM-manipulation/items.js
@@ -0,0 +1,34 @@
+const items = [
+    {
+        id: 1,
+        date: '10/2/2020',
+        type: 'utility bill',
+        desc: 'electricity bill',
+        amount: 66.00,
+        paymentType: 'debit card'
+    },
+    {
+        id: 2,
+        date: '10/3/2020',
+        type: 'food',
+        desc: 'Chipotle',
+        amount: 15.39,
+        paymentType: 'cash'
+    },
+    {
+        id: 3,
+        date: '10/3/2020',
+        type: 'food',
+        desc: 'Starbucks',
+        amount: 4.59,
+        paymentType: 'credit card'
+    },
+    {
+        id: 4,
+        date: '10/5/2020',
+        type: 'food',
+        desc: 'Grocery shopping',
+        amount: 75.45,
+        paymentType: 'debit card'
+    },
+]
