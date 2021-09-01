ac6dc6bb64cc8bdab76f3b637d76f177e4f694e4
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-09-01 10:16:29 -0700

    add lesson 3 starter code

---
+++ phase-3/03_object_oriented_ruby_continued/exercise/lib/appointment.rb
@@ -0,0 +1,58 @@
+# - refactor your `initialize` method to use keyword arguments
+# - add a `.all` method that returns all persisted appointments and create an `@@all` class variable to store them in
+#   - add a `#save` method which persists the `appointment` receiving the method call to `@@all`. This method should return the appointment we call it on.
+#   - add a `.create` method which accepts attributes as an argument, uses them to instantiate a new instance of the class, and invokes `.save` on the new instance
+#   - add a `.by_doctor(doctor)` method which accepts a doctor name as an argument and returns all appointments with that doctor.
+#   - add a `.by_patient(patient)` method which accepts a patient name as an argument and returns all appointments with that patient.
+class Appointment
+  attr_accessor :time, :doctor, :patient, :purpose, :notes, :canceled
+  
+  def initialize(time, doctor, patient, purpose)
+    @time = time
+    @doctor = doctor
+    @patient = patient
+    @purpose = purpose
+    @canceled = false
+  end
+
+  def cancel
+    @canceled = true
+  end
+
+  def print
+    puts ""
+    puts "#{self.time.light_green} #{self.canceled ? '(CANCELED)'.red : ''}"
+    puts "  Doctor: #{self.doctor}"
+    puts "  Patient: #{self.patient}"
+    puts "  Purpose: #{self.purpose}"
+    puts "  Notes: #{self.notes || 'none'}"
+    puts ""
+  end
+end
+
+# After you've reworked the initialize method and defined your create method, 
+# you can uncomment the lines below to use them as seed data for the application
+# so you won't have to create new appointments each time you want to test your 
+# application.
+
+
+# Appointment.create(
+#   time: "2:00 PM",
+#   doctor: "Dr. Drew",
+#   patient: "D",
+#   purpose: "Physical"
+# )
+
+# Appointment.create(
+#   time: "3:00 PM", 
+#   doctor: "Dr. Zhivago", 
+#   patient: "D", 
+#   purpose: "Checkup"
+# )
+
+# Appointment.create(
+#   time: "5:00 PM", 
+#   doctor: "Dr. Zhivago", 
+#   patient: "Laurence Fishburne", 
+#   purpose: "Checkup"
+# )
---
+++ phase-3/03_object_oriented_ruby_continued/exercise/lib/appointments_cli.rb
@@ -0,0 +1,109 @@
+APPOINTMENTS = []
+
+def start_cli
+  puts "Hi! Welcome to the Appointments CLI"
+  main_menu
+  handle_user_choice
+  puts "Thanks for using the appointments CLI!"
+end
+
+def main_menu
+  puts "Here's what you can do:".cyan
+  puts "  1. Add Appointment".cyan
+  puts "  2. List appointments".cyan
+  puts "  3. Add notes to appointment".cyan
+  puts "  4. Cancel appointment".cyan
+  puts "  5. View Appointments by Doctor".cyan
+  puts "  6. View Appointments by Patient".cyan
+  puts "  exit".cyan
+  puts "Type the number corresponding to your choice, or 'exit' to leave the program".cyan
+end
+
+def handle_user_choice
+  input = gets.chomp 
+  while input != "exit" do
+    if input == "1"
+      add_appointment
+    elsif input == "2"
+      APPOINTMENTS.each do |appointment|
+        appointment.print
+      end
+    elsif input == "3"
+      add_notes
+    elsif input == "4"
+      cancel_appointment
+    elsif input == "5"
+      appointments_by_doctor
+    elsif input == "6"
+      appointments_by_patient
+    elsif input == "debug"
+      binding.pry
+    else 
+      puts "Whoops! I didn't understand that command".red
+    end
+    main_menu
+    input = gets.chomp
+  end
+end
+
+def add_appointment
+  print "What time does the appointment start? "
+  time = ask_for_input
+  print "What doctor is the appointment with? "
+  doctor = ask_for_input
+  print "Which patient is this appointment for? "
+  patient = ask_for_input
+  print "What is the purpose of your visit? "
+  purpose = ask_for_input
+  appointment = Appointment.new(time, doctor, patient, purpose)
+  APPOINTMENTS << appointment
+  appointment.print
+  appointment
+end
+
+def add_notes
+  puts "Pick the number matching the appointment you would like to add notes to".cyan
+  APPOINTMENTS.each.with_index(1) do |appointment, index|
+    puts "#{index}. #{appointment.time} with #{appointment.doctor}"
+  end
+  input_to_index = ask_for_input.to_i - 1
+  appointment_to_notate = APPOINTMENTS[input_to_index]
+  puts "What notes would you like to add to the #{appointment_to_notate.time} appointment with #{appointment_to_notate.doctor}?"
+  notes = gets.chomp
+  appointment_to_notate.notes = notes
+  appointment_to_notate.print
+end
+
+def cancel_appointment
+  puts "Pick the number matching the appointment you would like to cancel".cyan
+  APPOINTMENTS.each.with_index(1) do |appointment, index|
+    puts "#{index}. #{appointment.time} with #{appointment.doctor}"
+  end
+  input_to_index = ask_for_input.to_i - 1
+  appointment_to_cancel = APPOINTMENTS[input_to_index]
+  appointment_to_cancel.cancel
+  appointment_to_cancel.print
+end
+
+def ask_for_input
+  input = gets.chomp
+  if input == "exit" 
+    puts "Thank you for using the appointments CLI."
+    exit
+  end
+  input
+end
+
+def appointments_by_doctor
+  # print out a list of doctor's names
+  # ask user to choose which doctor's appointments they want to view
+  # use your Appointment class method by_doctor to get the relevant
+  # appointments and call print on each
+end
+
+def appointments_by_patient
+  # print out a list of patient's names
+  # ask user to choose which doctor's appointments they want to view
+  # use your Appointment class method by_doctor to get the relevant
+  # appointments and call print on each
+end
