966f7e5d78e4a4bef6327b74053a3dfdfc926421
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-09-10 10:10:22 -0700

    remove walks controller from demo solution code

--- phase-3/08_intro_to_sinatra/demo/api/app/controllers/walks_controller.rb
+++ phase-3/08_intro_to_sinatra/demo/api/app/controllers/walks_controller.rb
@@ -1,22 +1,5 @@
 class WalksController < ApplicationController
 
-  get "/walks" do 
-    Walk.all.to_json(
-      methods: :formatted_time
-    )
-  end
- 
-  post "/walks" do 
-    Walk.create(walk_params).to_json(
-      methods: :formatted_time
-    )
-  end
-
-  delete "/walks/:id" do 
-    Walk.find(params[:id]).destroy.to_json(
-      methods: :formatted_time
-    )
-  end
 
   private 
 
@@ -24,7 +7,6 @@ class WalksController < ApplicationController
   # we use this method to create a list of what's permitted to be passed to .create or .update
   # within controller actions.
   def walk_params
-    allowed_params = %w(time dog_ids)
-    params.select {|param,value| allowed_params.include?(param)}
+    
   end
 end
