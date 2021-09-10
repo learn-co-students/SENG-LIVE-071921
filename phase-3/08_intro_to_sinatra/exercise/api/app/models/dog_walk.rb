dffb72910ef86fff4d055b6435f7598e77b08094
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-09-10 10:23:26 -0700

    update models in exercise starter for lesson 8

--- phase-3/08_intro_to_sinatra/exercise/api/app/models/dog_walk.rb
+++ phase-3/08_intro_to_sinatra/exercise/api/app/models/dog_walk.rb
@@ -4,9 +4,4 @@ class DogWalk < ActiveRecord::Base
 
   scope :with_poop, -> { where(pooped: true) }
 
-  delegate :formatted_time, to: :walk
-
-  def walk_time=(time)
-    self.walk = Walk.find_or_create_by(time: time)
-  end
 end
