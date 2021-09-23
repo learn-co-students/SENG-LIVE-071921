86fe63625b845973ed2fb076a47dae3afeb04d85
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-09-23 15:16:04 -0700

    add solution from lecture 4

--- phase-4/04_rails_serializers/meetup_clone_api/app/serializers/group_index_serializer.rb
+++ phase-4/04_rails_serializers/meetup_clone_api/app/serializers/group_index_serializer.rb
@@ -1,3 +1,6 @@
 class GroupIndexSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location
+  attributes :id, :name, :location, :user_group
+  def user_group
+    current_user.user_groups.find_by(group_id: object.id)
+  end
 end
